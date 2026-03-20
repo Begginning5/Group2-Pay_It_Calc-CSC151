d2a8ba7cee9d9f8b508bc7be964470a8b1613b42
cmobrien3 <140217500+Begginning5@users.noreply.github.com>
2026-03-20 00:30:30 -0400

    Update header comments in User_Formulas.js

--- User_Formulas.js
+++ User_Formulas.js
@@ -1,11 +1,12 @@
-//===========================
-// Date:
-// Author:
-// Second Author:
-// Function Name:
-// [Y/N] Approved? (username1);
-// Description:
-//============================
+# Please Update your Username
+#===========================
+# Date:
+# Author:
+# Second Author:
+# Function Name:
+# [Y/N] Approved? (username1);
+# Description:
+#============================
 
 function endOfClassMessage() {
   console.log("Welcome to the end of the class.");
