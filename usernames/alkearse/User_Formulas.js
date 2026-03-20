88fe8cdbb9ae72a4a45d908613ec44997856f74e
cmobrien3 <140217500+Begginning5@users.noreply.github.com>
2026-03-20 00:31:28 -0400

    Update
    Added a template for user information and a welcome message function.

--- usernames/alkearse/User_Formulas.js
+++ usernames/alkearse/User_Formulas.js
@@ -1,3 +1,15 @@
 // TODO: Please enter your username here:
 
-// (Original content of User_Formulas.js goes here)
+# Please Update your Username
+#===========================
+# Date:
+# Author:
+# Second Author:
+# Function Name:
+# [Y/N] Approved? (username1);
+# Description:
+#============================
+
+function endOfClassMessage() {
+  console.log("Welcome to the end of the class.");
+}
