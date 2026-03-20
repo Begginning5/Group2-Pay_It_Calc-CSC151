8bd233d424c893e91e5166657f16105b6bc3ba48
cmobrien3 <140217500+Begginning5@users.noreply.github.com>
2026-03-20 00:45:55 -0400

    Add user metadata and endOfClassMessage function
    Added metadata section for user information and a function to log a message.

--- Group2_Formulas.js
+++ Group2_Formulas.js
@@ -1,2 +1,16 @@
 // Contents of Group2_Formulas.js - previously User_Formulas.js
-// Your existing JavaScript code here...
+// TODO: Please enter your username here:
+
+# Please Update your Username
+#===========================
+# Date:
+# Author:
+# Second Author:
+# Function Name:
+# [Y/N] Approved? (username1);
+# Description:
+#============================
+
+function endOfClassMessage() {
+  console.log("Welcome to the end of the class.");
+}
