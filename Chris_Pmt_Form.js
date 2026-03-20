1f53058f05a21dc8431cd29ff76552f525493213
cmobrien3 <140217500+Begginning5@users.noreply.github.com>
2026-03-19 20:49:43 -0400

    cmobrien3 added header to formula file.
    Initial commit of Chris_Pmt_Form.js with header comments.

---
+++ Chris_Pmt_Form.js
@@ -0,0 +1,15 @@
+//================================================================================================
+// Author: Christopher O'Brien (03.19.26)
+// Editors: 
+//------------------------------------------------------------------------------------------------
+// Testing:
+// [] Functional (Y/N)
+// [] Integration (Y/N)
+// [] Acceptance (Y/N)
+// COMMENTS: {[cmobrien3]="Hello. Feel Free to Implement the Group2 Header I am using. 
+//                         Or atleast name and date!"}
+//------------------------------------------------------------------------------------------------
+// Description:
+// - This is the first of a series of formula files defined by cmobrien3 for JavaScript Final Project.
+//=================================================================================================
+
