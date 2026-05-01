8097408a5f5d12f79b9fdbde784fe27164d9fb17
cmobrien3 <140217500+Begginning5@users.noreply.github.com>
2026-05-01 18:14:20 -0400

    Update import statement for TestLoanClassTemplate

--- Main_Group2-Pay_It_Cal-CSC151.java
+++ Main_Group2-Pay_It_Cal-CSC151.java
@@ -1,4 +1,4 @@
-import groupproject2026.Loan, groupproject2026.TestLoanTemplate;
+import groupproject2026.Loan, groupproject2026.TestLoanClassTemplate;
 
 <contents of the existing file>
 System.out.println("This will be the file I submit.");
