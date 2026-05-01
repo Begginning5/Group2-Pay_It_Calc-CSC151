6d120f32b2971d0fa255cd27566b49ec5785bc41
cmobrien3 <140217500+Begginning5@users.noreply.github.com>
2026-05-01 18:16:22 -0400

    Add submission message to Main_Group2-Pay_It_Cal-CSC151.java

--- Main_Group2-Pay_It_Cal-CSC151.java
+++ Main_Group2-Pay_It_Cal-CSC151.java
@@ -1,5 +1,6 @@
+// Load the PROJECT TESTING PACKAGES
 import groupproject2026.Loan, groupproject2026.TestLoanClassTemplate;
-
+// Load the user functions
 <contents of the existing file>
 System.out.println("This will be the file I submit.");
 
