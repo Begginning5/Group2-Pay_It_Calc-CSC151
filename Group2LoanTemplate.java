df319116a756d1eb591c1003a6413fc89c7d6b78
cmobrien3 <140217500+Begginning5@users.noreply.github.com>
2026-05-01 09:58:29 -0400

    Add comments for loan-related functions
    Just trying to find all the stuff.

--- Group2LoanTemplate.java
+++ Group2LoanTemplate.java
@@ -21,37 +21,51 @@ public class LoanTemplate {
 	    this.loanAmount = loanAmount;
 	    loanDate = new java.util.Date();
 	  }
-
+	//-----------------------------------
+	// Function1:
+	//-----------------------------------
 	  /** Return annualInterestRate */
 	  public double getAnnualInterestRate() {
 	    return annualInterestRate;
 	  }
-
+	//------------------------------------
+	// Function2:
+	//------------------------------------
 	  /** Set a new annualInterestRate */
 	  public void setAnnualInterestRate(double annualInterestRate) {
 	    this.annualInterestRate = annualInterestRate;
 	  }
-
+	//-------------------------------------
+	// Function3:
+	//-------------------------------------
 	  /** Return numberOfYears */
 	  public int getNumberOfYears() {
 	    return numberOfYears;
 	  }
-
+	//-------------------------------------
+	// Function4:
+	//-------------------------------------
 	  /** Set a new numberOfYears */
 	  public void setNumberOfYears(int numberOfYears) {
 	    this.numberOfYears = numberOfYears;
 	  }
-
+	//---------------------------------------
+	// Function5:
+	//---------------------------------------
 	  /** Return loanAmount */
 	  public double getLoanAmount() {
 	    return loanAmount;
 	  }
-
+	//---------------------------------------
+	// Function6:
+	//---------------------------------------
 	  /** Set a newloanAmount */
 	  public void setLoanAmount(double loanAmount) {
 	    this.loanAmount = loanAmount;
 	  }
-
+	//---------------------------------------
+	// Function7:
+	//---------------------------------------
 	  /** Find monthly payment */
 	  public double getMonthlyPayment() {
 	    double monthlyInterestRate = annualInterestRate / 1200;
@@ -59,20 +73,26 @@ public class LoanTemplate {
 	      (1 / Math.pow(1 + monthlyInterestRate, numberOfYears * 12)));
 	    return monthlyPayment;    
 	  }
-
+	//-------------------------------------
+	// Function8:
+	//-------------------------------------
 	  /** Find total payment */
 	  public double getTotalPayment() {
 	    double totalPayment = getMonthlyPayment() * numberOfYears * 12;
 	    return totalPayment;    
 	  }
-	  
+	//-------------------------------------
+	// Function9:
+	//-------------------------------------
 	  // 11/9
 	  /** Find total interest */
 	  public double getTotalInterest() {
 	    double totalInterest = getTotalPayment() - loanAmount;
 	    return totalInterest;    
 	  }
-
+	//--------------------------------------
+	// Function10:
+	//--------------------------------------
 	  /** Return loan date */
 	  public java.util.Date getLoanDate() {
 	    return loanDate;
