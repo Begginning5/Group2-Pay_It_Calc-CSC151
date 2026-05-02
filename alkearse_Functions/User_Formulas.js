c23412f9b4e48af839c56eeddbd48cae72c99c1b
cmobrien3 <140217500+Begginning5@users.noreply.github.com>
2026-05-02 16:00:31 -0400

    File: Thinking
    Thinking

--- alkearse_Functions/User_Formulas.js
+++ alkearse_Functions/User_Formulas.js
@@ -1,1 +1,124 @@
+alkearse 
+
+// alkearse
+//===========================
+// Date:
+// Author:
+// Second Author:
+// Function Name:
+// [Y/N] Approved? (alkearse);
+// Description:
+//============================
+
+function endOfClassMessage() {
+  console.log("Welcome to the end of the class.");
+}
+
+function getMonthlyPayment(loan, rate, years) {
+
+  let monthlyRate = rate / 12 / 100;
+  let months = years * 12;
+
+  let payment =
+    loan
+    monthlyRate *
+    Math.pow(1 + monthlyRate, months) / 
+    (Math.pow(1 + monthlyRate, months) - 1);
+
+  return payment; 
+}
+
+
+function loanExtra(loan, rate, years) {
+  let monthlyRate = rate / 12 / 100;
+  let payment = getMonthlyPayment(loan, rate, years);
+
+  let balance = loan;
+  let totalInterest = 0;
+  let months = 0;
+
+  while (balance > 0) {
+
+    let interest = balance * monthlyRate;
+    let principal = payment - interest;
+
+    balance -= principal;
+    totalInterest += interest;
+
+    months++;
+
+    if (months > years * 12 * 2) break;
+  }
+
+  return {
+    interest: totalInterest,
+    months: months 
+
+  }; 
+}
+
+function loanFirstMonthExtra(loan, rate, years, extra) {
+
+  let monthlyRate = rate / 12 / 100;
+  let payment = getMonthlypayment(loan, rate, years);
+
+  let balance = loan;
+  let totalInterest = 0;
+  let months = 0;
+
+  while (balance > 0) {
+
+    let interest = balance 
+  }
+}
+
+function loanFirstMonthExtra(loan, rate, years, extra) {
+  let monthlyRate = rate / 12 / 100;
+  let payment = getMonthlyPayment(loan, rate, years);
+
+  let balance = loan;
+  let totalInterest = 0;
+  let months = 0;
+
+  while (balance > 0) {
+    let interest = balance * monthlyRate;
+
+    let actualPayment = payment;
+    if (months === 0) {
+      actualPayment += extra;
+    }
+
+    let principal = actualPayment - interest;
+
+    balance -= principal;
+    totalInterest += interest;
+
+    months++;
+
+    if (months > years * 12 * 2) break;
+  }
+
+  return { 
+    interest: totalInterest,
+    months: months 
+
+  };
+}
+
+
+function loanMonthlyExtra(loan, rate, years,extra) {
+
+  let monthlyRate = rate / 12/ 100;
+
+  let payment = getMonthlyPayment(loan, rate, years);
+
+  let balance = loan; 
+  let totalinterest = 0;
+  let months = 0;
+
+  while(balance > 0) {
+
+    let 
+  }
+}
 
