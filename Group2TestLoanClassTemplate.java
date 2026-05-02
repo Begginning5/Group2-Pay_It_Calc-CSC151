1091b3ccd325569bec388b888e521e2a7d2d275e
cmobrien3 <140217500+Begginning5@users.noreply.github.com>
2026-05-02 00:34:03 -0400

    Refactor user function loading and prompts

--- Group2TestLoanClassTemplate.java
+++ Group2TestLoanClassTemplate.java
@@ -1,11 +1,19 @@
-package groupproject2026;
-// LiveExample 10.1
-import java.util.Scanner;
-import alkearse from "./usernames/alkearse/User_Formulas.js";
-import aschrader from "./usernames/aschrader/User_Formulas.js";
-import cethompson7 from "./usernames/cethompson7/User_Formulas.js";
-import cfrederic from "./usernames/cfrederic/User_Formulas.js";
-import cmobrien3 from "./usernames/cmobrien3/User_Formulas.js";
+// Load the PROJECT TESTING PACKAGES
+import Fgroupproject2026.LoanTemplate;
+// USER Project Package
+import alkearse.alkerse;
+import aschrader.aschrader;
+import cethompson7.cethompson7;
+import cfrederic.cfrederic;
+import cmobrien3.cmobrien3;
+// Load the USER FUNCTIONS FROM FILES
+//import usernames.alkearse.user_Functions.java;
+//import usernames.aschrader.user_Functions.java;
+//import usernames.cethompson7.user_Functions.java;
+//import usernames.cfrederic.user_Functions.java;
+//import usernames.cmobrien3.user_Functions.java;
+// GENERAL Java import statements
+//import java.util.Scanner;
 
 public class TestLoanClassTemplate {
   /** Main method */
@@ -13,12 +21,15 @@ public class TestLoanClassTemplate {
 	// Create a Scanner
     Scanner input = new Scanner(System.in);
 
-    System.out.println("Enter the username for your waketech account to run your formulas: ");
+    System.out.println("Enter the username to run the user functions (alkearse, aschrader, cethompson7, cfrederic, cmobrien3): ");
+    System.out.println("To run the default functions enter Fgroupproject2026");
     String username = input.nextLine().trim();
+    boolean runDefault = username.equals("Fgroupproject2026");
+    System.out.println();
+    System.out.println("NEXT:");
 
     // Enter yearly interest rate
-    System.out.print(
-      "Enter annual interest rate, for example, 8.25: ");
+    System.out.print("Enter annual interest rate, for example, 8.25: ");
     double annualInterestRate = input.nextDouble();
 
     // Enter number of years
@@ -29,14 +40,13 @@ public class TestLoanClassTemplate {
     System.out.print("Enter loan amount, for example, 120000.95: ");
     double loanAmount =  input.nextDouble();
     
-    //11/9
+    //11/9 -- GOSH! 
     // Enter extra payment amount
     System.out.print("Enter extra payment amount, for example, 1000.00: ");
     double extraPaymentAmount =  input.nextDouble();
 
     // Create Loan object
-    Loan loan =
-      new Loan(annualInterestRate, numberOfYears, loanAmount);
+    Loan loan = new Loan(annualInterestRate, numberOfYears, loanAmount);
 
     // Display loan date, monthly payment, and total payment, and total interest
     System.out.printf("The loan was created on %s%n" +
@@ -49,33 +59,38 @@ public class TestLoanClassTemplate {
     extraPayFirstMonth(loan, extraPaymentAmount);
     System.out.println();
     extraPayEveryMonth(loan, extraPaymentAmount);
-  }
   
+  //------------------------------
+  // Function11: extraPayFirstMonth
+  //------------------------------
   /** Extra payment first month only */
-  	try {
-  		// Build a class name dynamically
-  		String className = "usernames." + username + ".User_Formulas";
-  		// Load the class
-		Class<?> userClass = Class.forName(className);
-		// Get the method you want to call
-		var method = userClass.getMethod("extraPayFirstMonth", Loan.class, double.class);
-		// Call the method 
-		Object result = method.invoke(null, loan, extraPaymentAmount);
-		// Print the result
-		System.out.println("Result: " + result);
-		// Catch the exception.
-	} catch (Exception e) {System.out.println("Could not find a method for that username.");}
 
-  	try {
-  		// Build a class name dynamically
-  		String className = "usernames." + username + ".User_Formulas";
+  	{try {
+  		// Build a class name dynamically -- I think the default is good.  
+      String className = (runDefault == true) ? "Fgroupproject2026.LoanTemplate" : username + "." + username;
   		// Load the class
-		Class<?> userClass = Class.forName(className);
-		// Get the method you want to call
-		var method = userClass.getMethod("extraPayEveryMonth", Loan.class, double.class);
-		// Call the method 
-		Object result = method.invoke(null, loan, extraPay);
-		// Print the result
-		System.out.println("Result: " + result);
-		// Catch the exception.
-	} catch (Exception e) {System.out.println("Could not find a method for that username.");}
+		  Class<?> userClass = Class.forName(className);
+		  // Get the method you want to call
+		  var method = userClass.getMethod("extraPayFirstMonth", Loan.class, double.class);
+		  // Call the method 
+		  Object result = method.invoke(null, loan, extraPaymentAmount);
+		  // Print the result
+		  System.out.println("Result: " + result);
+		  // Catch the exception.
+	} catch (Exception e) {System.out.println("Could not find a method for that username.");}}
+  //------------------------------
+  // Function12: extraPayEveryMonth
+  //------------------------------
+  	{try {
+  		// Build a class name dynamically
+      String className = (runDefault == true) ? "Fgroupproject2026.LoanTemplate" : username + "." + username;
+  		// Load the class
+		  Class<?> userClass = Class.forName(className);
+		  // Get the method you want to call
+		  var method = userClass.getMethod("extraPayEveryMonth", Loan.class, double.class);
+		  // Call the method 
+		  Object result = method.invoke(null, loan, extraPaymentAmount);
+		  // Print the result
+		  System.out.println("Result: " + result);
+		  // Catch the exception.
+	} catch (Exception e) {System.out.println("Could not find a method for that username.");}}}}
