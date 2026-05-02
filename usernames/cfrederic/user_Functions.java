a4eeacad57cc1ee4eab6617d0e8b6d474d6f78b2
cmobrien3 <140217500+Begginning5@users.noreply.github.com>
2026-05-02 00:35:24 -0400

    Refactor user_Functions.java and update class structure

--- usernames/cfrederic/user_Functions.java
+++ usernames/cfrederic/user_Functions.java
@@ -1,15 +1,89 @@
-# Please Update Your Username 
+package cfrederic;
+//===========================
+// Date:
+// Author:
+// Second Author:
+// Function Name:
+// [Y/N] Approved? (username1);
+// Description:
+//============================
 
-# Please Update your Username
-#===========================
-# Date:
-# Author:
-# Second Author:
-# Function Name:
-# [Y/N] Approved? (username1);
-# Description:
-#============================
+public class cfrederic{
+	// LiveExample 10.2
+	private double annualInterestRate;
+	private int numberOfYears;
+	private double loanAmount;
+	private java.util.Date loanDate;
 
-function endOfClassMessage() {
-  console.log("Welcome to the end of the class.");
-}
+	/** No-arg constructor */
+	public LoanTemplate() {
+		this(2.5, 1, 1000);
+	}
+
+	/** Construct a loan with specified annual interest rate,
+		 number of years, and loan amount
+		*/
+	public LoanTemplate(double annualInterestRate, int numberOfYears,
+		double loanAmount) {
+		this.annualInterestRate = annualInterestRate;
+		this.numberOfYears = numberOfYears;
+		this.loanAmount = loanAmount;
+		loanDate = new java.util.Date();
+	}
+
+	/** Return annualInterestRate */
+	public double getAnnualInterestRate() {
+		return annualInterestRate;
+	}
+
+	/** Set a new annualInterestRate */
+	public void setAnnualInterestRate(double annualInterestRate) {
+		this.annualInterestRate = annualInterestRate;
+	}
+
+	/** Return numberOfYears */
+	public int getNumberOfYears() {
+		return numberOfYears;
+	}
+		/** Set a new numberOfYears */
+	public void setNumberOfYears(int numberOfYears) {
+		this.numberOfYears = numberOfYears;
+	}
+
+	/** Return loanAmount */
+	public double getLoanAmount() {
+		return loanAmount;
+	}
+
+	/** Set a newloanAmount */
+	public void setLoanAmount(double loanAmount) {
+		this.loanAmount = loanAmount;
+	}
+
+	/** Find monthly payment */
+	public double getMonthlyPayment() {
+		double monthlyInterestRate = annualInterestRate / 1200;
+		double monthlyPayment = loanAmount * monthlyInterestRate / (1 -
+		(1 / Math.pow(1 + monthlyInterestRate, numberOfYears * 12)));
+		return monthlyPayment;    
+	}
+
+	/** Find total payment */
+	public double getTotalPayment() {
+		double totalPayment = getMonthlyPayment() * numberOfYears * 12;
+		return totalPayment;    
+	}
+		
+	// 11/9
+	/** Find total interest */
+	public double getTotalInterest() {
+		double totalInterest = getTotalPayment() - loanAmount;
+		return totalInterest;    
+	}
+
+	/** Return loan date */
+	public java.util.Date getLoanDate() {
+		return loanDate;
+	}
+		
+	}
